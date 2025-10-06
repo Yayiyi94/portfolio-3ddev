2dc0e1ccc3cfd1e3284781e635bcd5b253b1c92b
Yayiyi94 <yadiratr94@gmail.com>
2025-10-06 17:42:00 -0600

    Updated Hero: make buttons work

--- src/Hero.js
+++ src/Hero.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
 import { Container, Button } from "react-bootstrap";
+import ArtPortfolio from "./ArtPortfolio";
+import { Link } from "react-router-dom";
 import imageGirl from "./images/Captura de pantalla 2025-09-26 171123.png";
 
 import "./App.css";
@@ -19,16 +21,22 @@ export default function Hero() {
           about my two passions: the 3D world and computing{" "}
           <i class="bi bi-display text-danger"></i>
         </p>
-        <Button href="#about" variant="dark border" size="lg" className="mt-4">
-          About me
-        </Button>
+        <Link to="/about">
+          <Button variant="dark border" size="lg" className="mt-4">
+            About me
+          </Button>
+        </Link>
         <div class="mt-3 mb-2 m-3">
-          <Button href="#" variant="dark border" size="md" className="mx-4">
-            See 3D Portfolio
-          </Button>
-          <Button href="#" variant="dark border" size="md" className="mx-4">
-            See Dev Portfolio
-          </Button>
+          <Link to="/art">
+            <Button variant="dark border" size="md" className="mx-4">
+              See 3D Portfolio
+            </Button>
+          </Link>
+          <Link to="/dev">
+            <Button variant="dark border" size="md" className="mx-4">
+              See Dev Portfolio
+            </Button>
+          </Link>
         </div>
         <div class="illustration">
           <img src={imageGirl} alt="Girl with laptop illustration" />
